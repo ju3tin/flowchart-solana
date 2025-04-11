db3c94598035a550ed4688d4b11193648be9e116
ju3tin <ju3tin@hotmail.co.uk>
2025-04-11 23:04:38 +0100

    sdf

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,5 @@
-import connect from '../../lib/mongodb';
-import User from '../../models/User';
+import connect from '../../../lib/mongodb';
+import User from '../../../models/User';
 import bcrypt from 'bcryptjs';
 
 export default async function handler(req, res) {
