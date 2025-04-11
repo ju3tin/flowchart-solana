07b803d62e1751f4ccfca690142e77e36dc55156
ju3tin <ju3tin@hotmail.co.uk>
2025-04-11 18:15:29 +0100

    sf

---
+++ pages/api/status.js
@@ -0,0 +1,9 @@
+   // pages/api/status.js
+   export default function handler(req, res) {
+    if (req.method === 'GET') {
+        res.status(200).json({ message: 'Server is running' });
+    } else {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
