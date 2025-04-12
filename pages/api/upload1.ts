6582320a680e2b9252f44e950311e3678f417cec
ju3tin <ju3tin@hotmail.co.uk>
2025-04-12 01:26:50 +0100

    sdf

--- pages/api/upload1.ts
+++ pages/api/upload1.ts
@@ -13,16 +13,19 @@ export async function POST(req: Request) {
     )
   }
 
-  const file = req.body || ''
+  const body = await req.blob()
+  const arrayBuffer = await body.arrayBuffer()
+  const fileBuffer = Buffer.from(arrayBuffer)
+
   const filename = req.headers.get('x-vercel-filename') || 'file.txt'
   const contentType = req.headers.get('content-type') || 'text/plain'
   const fileType = `.${contentType.split('/')[1]}`
 
-  // construct final filename based on content-type if not provided
   const finalName = filename.includes(fileType)
     ? filename
     : `${filename}${fileType}`
-  const blob = await put(finalName, file, {
+
+  const blob = await put(finalName, fileBuffer, {
     contentType,
     access: 'public'
   })
